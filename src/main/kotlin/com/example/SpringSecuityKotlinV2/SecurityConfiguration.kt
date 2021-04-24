5787f1a88774ce99f692450cae5bd46b87ae1be3
zubair-pc\zubair <szubair@gmail.com>
2021-04-24 18:05:25 +0500

    Basic in mempry authentication is proivded by using WebSecuityConfiguration adapter

--- src/main/kotlin/com/example/SpringSecuityKotlinV2/SecurityConfiguration.kt
+++ src/main/kotlin/com/example/SpringSecuityKotlinV2/SecurityConfiguration.kt
@@ -1,7 +1,27 @@
 package com.example.SpringSecuityKotlinV2
 
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
+@EnableWebSecurity
 class SecurityConfiguration:WebSecurityConfigurerAdapter() {
-
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+       //Set the confiugration
+        auth?.inMemoryAuthentication()
+                ?.withUser("root")
+                ?.password("root")
+                ?.roles("User")
+                ?.and()
+                ?.withUser("admin")
+                ?.password("admin")
+                ?.roles("Admin")
+    }
+    @Bean
+    fun getPasswordEncoder():PasswordEncoder{
+        return NoOpPasswordEncoder.getInstance()
+    }
 }
